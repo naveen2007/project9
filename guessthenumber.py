8e0b5ca30f53844ba61e13960194407d7e09d93f
naveen2007 <72682781+naveen2007@users.noreply.github.com>
2021-09-05 18:47:21 -0700

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,7 +1,7 @@
-guessNumber=int(input("guess a number between 1 to 9"))
-if(guessNumber>5):
+guess=int(input("guess between 1 to 9"))
+if(guess>5):
     print("too high")
-elif(guessNumber<5):
+elif(guess<5):
     print("too low")
 else:
     print("correct")
